a9a06803d04a5a90e7f4b12aabae2b3c93e9ec1b
Damien Hoareau <damien.hoareau@gmail.com>
2023-05-26 13:09:58 +0400

    Fix code smell

--- Mercadona.Backend/Data/Migrations/20230506040707_MercadonaModels.cs
+++ Mercadona.Backend/Data/Migrations/20230506040707_MercadonaModels.cs
@@ -1,32 +1,34 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Mercadona.Backend.Data.Migrations
+namespace Mercadona.Backend.Data.Migrations;
+
+/// <inheritdoc />
+public partial class MercadonaModels : Migration
 {
     /// <inheritdoc />
-    public partial class MercadonaModels : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Offers",
-                columns: table => new
+        migrationBuilder.CreateTable(
+            name: "Offers",
+            columns: table =>
+                new
                 {
                     StartDate = table.Column<DateOnly>(type: "date", nullable: false),
                     EndDate = table.Column<DateOnly>(type: "date", nullable: false),
                     Percentage = table.Column<int>(type: "integer", nullable: false)
                 },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Offers", x => new { x.StartDate, x.EndDate, x.Percentage });
-                });
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Offers", x => new { x.StartDate, x.EndDate, x.Percentage });
+            }
+        );
 
-            migrationBuilder.CreateTable(
-                name: "Products",
-                columns: table => new
+        migrationBuilder.CreateTable(
+            name: "Products",
+            columns: table =>
+                new
                 {
                     Id = table.Column<Guid>(type: "uuid", nullable: false),
                     Label = table.Column<string>(type: "text", nullable: false),
@@ -35,54 +37,60 @@ namespace Mercadona.Backend.Data.Migrations
                     Image = table.Column<byte[]>(type: "bytea", nullable: false),
                     Category = table.Column<string>(type: "text", nullable: false)
                 },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Products", x => x.Id);
-                });
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Products", x => x.Id);
+            }
+        );
 
-            migrationBuilder.CreateTable(
-                name: "OfferProduct",
-                columns: table => new
+        migrationBuilder.CreateTable(
+            name: "OfferProduct",
+            columns: table =>
+                new
                 {
                     ProductsId = table.Column<Guid>(type: "uuid", nullable: false),
                     OffersStartDate = table.Column<DateOnly>(type: "date", nullable: false),
                     OffersEndDate = table.Column<DateOnly>(type: "date", nullable: false),
                     OffersPercentage = table.Column<int>(type: "integer", nullable: false)
                 },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_OfferProduct", x => new { x.ProductsId, x.OffersStartDate, x.OffersEndDate, x.OffersPercentage });
-                    table.ForeignKey(
-                        name: "FK_OfferProduct_Offers_OffersStartDate_OffersEndDate_OffersPer~",
-                        columns: x => new { x.OffersStartDate, x.OffersEndDate, x.OffersPercentage },
-                        principalTable: "Offers",
-                        principalColumns: new[] { "StartDate", "EndDate", "Percentage" },
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_OfferProduct_Products_ProductsId",
-                        column: x => x.ProductsId,
-                        principalTable: "Products",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+            constraints: table =>
+            {
+                table.PrimaryKey(
+                    "PK_OfferProduct",
+                    x =>
+                        new { x.ProductsId, x.OffersStartDate, x.OffersEndDate, x.OffersPercentage }
+                );
+                table.ForeignKey(
+                    name: "FK_OfferProduct_Offers_OffersStartDate_OffersEndDate_OffersPer~",
+                    columns: x => new { x.OffersStartDate, x.OffersEndDate, x.OffersPercentage },
+                    principalTable: "Offers",
+                    principalColumns: new[] { "StartDate", "EndDate", "Percentage" },
+                    onDelete: ReferentialAction.Cascade
+                );
+                table.ForeignKey(
+                    name: "FK_OfferProduct_Products_ProductsId",
+                    column: x => x.ProductsId,
+                    principalTable: "Products",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade
+                );
+            }
+        );
 
-            migrationBuilder.CreateIndex(
-                name: "IX_OfferProduct_OffersStartDate_OffersEndDate_OffersPercentage",
-                table: "OfferProduct",
-                columns: new[] { "OffersStartDate", "OffersEndDate", "OffersPercentage" });
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_OfferProduct_OffersStartDate_OffersEndDate_OffersPercentage",
+            table: "OfferProduct",
+            columns: new[] { "OffersStartDate", "OffersEndDate", "OffersPercentage" }
+        );
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "OfferProduct");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(name: "OfferProduct");
 
-            migrationBuilder.DropTable(
-                name: "Offers");
+        migrationBuilder.DropTable(name: "Offers");
 
-            migrationBuilder.DropTable(
-                name: "Products");
-        }
+        migrationBuilder.DropTable(name: "Products");
     }
 }
--- Mercadona.Backend/Pages/Error.cshtml.cs
+++ Mercadona.Backend/Pages/Error.cshtml.cs
@@ -2,35 +2,34 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Mercadona.Backend.Pages
+namespace Mercadona.Backend.Pages;
+
+/// <summary>
+/// Code behind de la page Error
+/// </summary>
+/// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+[IgnoreAntiforgeryToken]
+public class ErrorModel : PageModel
 {
     /// <summary>
-    /// Code behind de la page Error
+    /// Obtient ou définit l'identifiant de la requête.
     /// </summary>
-    /// <seealso cref="Microsoft.AspNetCore.Mvc.RazorPages.PageModel" />
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    [IgnoreAntiforgeryToken]
-    public class ErrorModel : PageModel
+    public string? RequestId { get; set; }
+
+    /// <summary>
+    /// Obtient une valeur booléenne indiquant si l'identifiant de la requête doit être affiché.
+    /// </summary>
+    /// <value>
+    ///   <c>true</c> if [show request identifier]; otherwise, <c>false</c>.
+    /// </value>
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    /// <summary>
+    /// Exécutée lors de la récupération de la page.
+    /// </summary>
+    public void OnGet()
     {
-        /// <summary>
-        /// Obtient ou définit l'identifiant de la requête.
-        /// </summary>
-        public string? RequestId { get; set; }
-
-        /// <summary>
-        /// Obtient une valeur booléenne indiquant si l'identifiant de la requête doit être affiché.
-        /// </summary>
-        /// <value>
-        ///   <c>true</c> if [show request identifier]; otherwise, <c>false</c>.
-        /// </value>
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        /// <summary>
-        /// Exécutée lors de la récupération de la page.
-        /// </summary>
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
 }
